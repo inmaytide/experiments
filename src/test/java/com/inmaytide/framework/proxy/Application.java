11cfe67dba2c83e3b23126f5b831923f74b676d0
inmaytide <inmaytide@gmail.com>
2016-03-10 10:01:19 +0800

    20160310 cglib动态代理

--- src/test/java/com/inmaytide/framework/proxy/Application.java
+++ src/test/java/com/inmaytide/framework/proxy/Application.java
@@ -3,10 +3,13 @@ package com.inmaytide.framework.proxy;
 public class Application {
 
 	public static void main(String[] args) {
-		IHello hello = new HelloImpl();
-		DynamicProxy proxy = new DynamicProxy(hello);
-		IHello helloProxy = proxy.getProxy();
-		helloProxy.say("Chris");
+//		IHello hello = new HelloImpl();
+//		DynamicProxy proxy = new DynamicProxy(hello);
+//		IHello helloProxy = proxy.getProxy();
+//		helloProxy.say("Chris");
+		
+		HelloImpl helloProxy = CGLibProxy.getInstance().getProxy(HelloImpl.class);
+		helloProxy.say("Jack");
 	}
 
 }
