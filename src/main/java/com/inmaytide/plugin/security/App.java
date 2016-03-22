0645333819c72a94d6ae2faeaec25c183afbdcab
inmaytide <inmaytide@gmail.com>
2016-03-22 21:48:46 +0800

    20160322 -242

---
+++ src/main/java/com/inmaytide/plugin/security/App.java
@@ -0,0 +1,13 @@
+package com.inmaytide.plugin.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
