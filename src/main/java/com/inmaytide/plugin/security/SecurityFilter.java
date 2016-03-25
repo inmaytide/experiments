f7e30919c005affe5935441fd40492af531a14d2
inmaytide <inmaytide@gmail.com>
2016-03-25 14:35:07 +0800

    20160325 -244

--- src/main/java/com/inmaytide/plugin/security/SecurityFilter.java
+++ src/main/java/com/inmaytide/plugin/security/SecurityFilter.java
@@ -3,5 +3,10 @@ package com.inmaytide.plugin.security;
 import org.apache.shiro.web.servlet.ShiroFilter;
 
 public class SecurityFilter extends ShiroFilter {
-
+	
+	@Override
+	public void init() throws Exception {
+		
+	}
+	
 }
